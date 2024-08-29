e7b5eabaac3625e6db134e4b0e083b1d7a5b9f0b
rishalism <rishalmuhammedup@gmail.com>
2024-08-29 17:58:54 +0530

    cleared minor bugs

--- src/infrasctructure/databases/OtpModel.ts
+++ src/infrasctructure/databases/OtpModel.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
 import { Otp } from "../../domain/OtpInterface";
 import { OTP_TIMER } from "../constants/jwt";
 
